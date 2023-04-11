28f34481396e28d84d1799dcb5f85871c20ec3cb
nmorgan8 <nmorgan8@lion.lmu.edu>
2023-04-11 13:31:41 -0700

    working pipeline

--- models.py
+++ models.py
@@ -1,26 +1,22 @@
 import tensorflow as tf
 from tensorflow_examples.models.pix2pix import pix2pix
-import os
+import matplotlib.pyplot as plt
 
 
-def get_unet():
+def get_model():
     generator_g = pix2pix.unet_generator(3, norm_type='instancenorm')
     generator_f = pix2pix.unet_generator(3, norm_type='instancenorm')
 
     discriminator_x = pix2pix.discriminator(norm_type='instancenorm', target=False)
     discriminator_y = pix2pix.discriminator(norm_type='instancenorm', target=False)
 
-    generator_g_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
-    generator_f_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
+    generator_g_optimizer = tf.keras.optimizers.legacy.Adam(2e-4, beta_1=0.5)
+    generator_f_optimizer = tf.keras.optimizers.legacy.Adam(2e-4, beta_1=0.5)
 
-    discriminator_x_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
-    discriminator_y_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
+    discriminator_x_optimizer = tf.keras.optimizers.legacy.Adam(2e-4, beta_1=0.5)
+    discriminator_y_optimizer = tf.keras.optimizers.legacy.Adam(2e-4, beta_1=0.5)
 
-    return generator_g, generator_f, discriminator_x, discriminator_y, generator_g_optimizer, generator_f_optimizer, discriminator_x_optimizer, discriminator_y_optimizer
-
-generator_g, generator_f, discriminator_x, discriminator_y, generator_g_optimizer, generator_f_optimizer, discriminator_x_optimizer, discriminator_y_optimizer = get_unet()
-
-ckpt = tf.train.Checkpoint(generator_g=generator_g,
+    ckpt = tf.train.Checkpoint(generator_g=generator_g,
                            generator_f=generator_f,
                            discriminator_x=discriminator_x,
                            discriminator_y=discriminator_y,
@@ -29,12 +25,19 @@ ckpt = tf.train.Checkpoint(generator_g=generator_g,
                            discriminator_x_optimizer=discriminator_x_optimizer,
                            discriminator_y_optimizer=discriminator_y_optimizer)
 
-checkpoint_path = "./rococo_checkpoints/train/ckpt-11"
-print(generator_g)
+    checkpoint_path = "./rococo_checkpoints/train/ckpt-11"
 
-# print(type(ckpt))
-# print(ckpt)
+    ckpt.restore(checkpoint_path).expect_partial()
 
-ckpt.restore(checkpoint_path).expect_partial()
+    return generator_g, generator_f, discriminator_x, discriminator_y, generator_g_optimizer, generator_f_optimizer, discriminator_x_optimizer, discriminator_y_optimizer
 
-print(generator_g)
+# normalizing the images to [-1, 1]
+def normalize(image):
+    image = tf.cast(image, tf.float32)
+    image = (image / 127.5) - 1
+    return image
+
+def unnormalize(image):
+    image = (image + 1) * 127.5
+    image = tf.cast(image, tf.uint8)
+    return image
--- run.py
+++ run.py
@@ -3,13 +3,22 @@ import numpy as np
 import tensorflow as tf
 import models
 
-def generate_image(image, file, art, choice):
-  return image
+generator_g, generator_f, discriminator_x, discriminator_y, generator_g_optimizer, generator_f_optimizer, discriminator_x_optimizer, discriminator_y_optimizer = models.get_model()
+
+def generate_image(inp, file, art, choice):
+    inp = inp.reshape((-1, 256, 256, 3))
+    inp = models.normalize(inp)
+    image = generator_g(inp)
+    image = models.unnormalize(image)
+    return image.numpy()[0]
+
+def display_img(inp, file, art, choice):
+    return inp
 
 demo = gr.Interface(
     fn=generate_image,
     inputs=[
-        gr.Image(source='webcam', tool=None),
+        gr.Image(source='webcam', tool=None, shape=(256, 256)),
         gr.UploadButton("Click to Upload a File", file_types=["image"]),
         gr.Dropdown(["Rococo", "Romanticism", "Impressionism", "Realism"], label="Art Period"),
         gr.Dropdown(["CycleGANs (UNET)", "CycleGANs (ResNet)", "VGG"], label="Model Choice")
