0c0bd3111a3595b79b5e7c0d00ae979666a3527f
Nick Morgan <nmorgan8@lion.lmu.edu>
2023-04-11 18:31:14 -0700

    file restructure

--- scripts/CycleGAN/cycle_gan_model.py
+++ scripts/CycleGAN/cycle_gan_model.py
@@ -31,6 +31,10 @@ def get_model():
 
     return generator_g, generator_f, discriminator_x, discriminator_y, generator_g_optimizer, generator_f_optimizer, discriminator_x_optimizer, discriminator_y_optimizer
 
+def get_vgg(style_img):
+    pass
+
+
 # normalizing the images to [-1, 1]
 def normalize(image):
     image = tf.cast(image, tf.float32)
