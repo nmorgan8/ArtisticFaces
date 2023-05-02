b6f015df3e88cbc9ae970e117f37acab35226d59
nmorgan8 <nmorgan8@lion.lmu.edu>
2023-05-01 17:01:29 -0700

    tabbed interface

--- src/run.py
+++ src/run.py
@@ -7,8 +7,15 @@ from scripts.VGG.test import get_model, tensor_to_image, load_input, get_style
 generator_g, _, _, _, _, _, _, _ = cycle_gan_model.get_model()
 vgg_model = get_model()
 
+examples = [
+    ['data/impressionism_vgg_sample.jpeg', 'Impressionism'],
+    ['data/rococo_vgg_sample.jpeg', 'Rococo'],
+    ['data/romanticism_vgg_sample.jpeg', 'Romanticism'],
+    ['data/realism_vgg_sample.jpeg', 'Realism'],
+]
 
-def generate_image(inp, file, art, choice):
+
+def generate_webcam(inp, art, choice):
     if choice == "VGG":
         inp = load_input(inp)
         style_img = get_style(art)
@@ -21,19 +28,52 @@ def generate_image(inp, file, art, choice):
         image = generator_g(inp)
         image = cycle_gan_model.unnormalize(image)
         return image.numpy()[0]
+    
+def generate_file(file, art, choice):
+    if choice == "VGG":
+        file = load_input(file)
+        style_img = get_style(art)
+        stylized_image = vgg_model(tf.constant(file), tf.constant(style_img))[0]
+        return tensor_to_image(stylized_image)
 
-demo = gr.Interface(
-    fn=generate_image,
+    elif choice == "CycleGANs (UNET)":
+        file = cycle_gan_model.normalize(file)
+        file = tf.expand_dims(file, axis=0)
+        image = generator_g(file)
+        image = cycle_gan_model.unnormalize(image)
+        return image.numpy()[0]
+
+webcam = gr.Interface(
+    fn=generate_webcam,
     inputs=[
-        gr.Image(source='webcam', tool=None, shape=None),
-        gr.UploadButton("Click to Upload a File", file_types=["image"]),
-        gr.Dropdown(["Rococo", "Romanticism", "Impressionism", "Realism"], label="Art Period"),
-        gr.Dropdown(["CycleGANs (UNET)", "CycleGANs (ResNet)", "VGG"], label="Model Choice")
+        gr.Image(source='webcam', label='Style Image'),
+        gr.Dropdown([ "Impressionism", "Rococo", "Romanticism", "Realism"], label="Art Period"),
+        gr.Dropdown(["VGG", "CycleGANs (UNET)"], label="Model Choice")
     ],
     outputs=[
         "image"
-    ]
+    ],
+    title="Artistic Faces",
+    description='<p style="text-align: center;">Transform your face and surroundings to your favorite art movement!</p>',
+    examples=examples
 )
+
+upload = gr.Interface(
+    fn=generate_file,
+    inputs=[
+        gr.Image(source='upload', label='Style Image'),
+        gr.Dropdown([ "Impressionism", "Rococo", "Romanticism", "Realism"], label="Art Period"),
+        gr.Dropdown(["VGG", "CycleGANs (UNET)"], label="Model Choice")
+    ],
+    outputs=[
+        "image"
+    ],
+    title="Artistic Faces",
+    description='<p style="text-align: center;">Transform your face and surroundings to your favorite art movement!</p>',
+    examples=examples
+)
+
+demo = gr.TabbedInterface([webcam, upload], ["Webcam", "Upload"])
 
 demo.launch(share=False)
 
